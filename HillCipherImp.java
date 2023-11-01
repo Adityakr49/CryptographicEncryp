6390cb52dc2f2ef00ebad16ca3df229ab18cd8fd
Adityakr49 <adityakumarpilot@gmail.com>
2023-11-01 22:56:00 +0530

    HillCipher

--- HillCipherImp.java
+++ HillCipherImp.java
@@ -1,30 +1,31 @@
-// Java code to implement Hill Cipher
 import java.util.Scanner;
+
 class HillCipherImp {
     static int det;
     static int detinv;
-    static void getKeyinv(int[][] matrix,int[][] keyinv) {
+
+    static void getKeyinv(int[][] matrix, int[][] keyinv) {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 int cofactor = calculateCofactor(matrix, i, j);
-                keyinv[j][i] = cofactor%26; // Transpose the cofactor matrix
-                if(keyinv[j][i]<0){
-                    keyinv[j][i]+=26;
+                keyinv[j][i] = cofactor % 26; // Transpose the cofactor matrix
+                if (keyinv[j][i] < 0) {
+                    keyinv[j][i] += 26;
                 }
             }
         }
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                keyinv[i][j]=(keyinv[i][j]*detinv)%26;
+                keyinv[i][j] = (keyinv[i][j] * detinv) % 26;
             }
         }
     }
-    
+
     static int calculateCofactor(int[][] matrix, int row, int col) {
         int[][] minorMatrix = new int[2][2];
         int minorRow = 0;
         int minorCol;
-        
+
         for (int i = 0; i < 3; i++) {
             if (i == row) {
                 continue;
@@ -39,75 +40,68 @@ class HillCipherImp {
             }
             minorRow++;
         }
-        
+
         int minorDeterminant = minorMatrix[0][0] * minorMatrix[1][1] - minorMatrix[0][1] * minorMatrix[1][0];
-        int cofactor = (int)Math.pow(-1, row + col) * minorDeterminant;
-        
+        int cofactor = (int) Math.pow(-1, row + col) * minorDeterminant;
+
         return cofactor;
     }
-    static int check(int[][] matrix){
-        detinv=-1;
+
+    static int check(int[][] matrix) {
+        detinv = -1;
         det = matrix[0][0] * (matrix[1][1] * matrix[2][2] - matrix[1][2] * matrix[2][1])
-                   - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
-                   + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
-        det%=26;
-        if(det<0)det+=26;
-        //mul inv of det mod 26
-        for(int i=0;i<25;i++){
-            if((det*i)%26==1){
+                - matrix[0][1] * (matrix[1][0] * matrix[2][2] - matrix[1][2] * matrix[2][0])
+                + matrix[0][2] * (matrix[1][0] * matrix[2][1] - matrix[1][1] * matrix[2][0]);
+        det %= 26;
+        if (det < 0) det += 26;
+        // Multiplicative inverse of det mod 26
+        for (int i = 0; i < 25; i++) {
+            if ((det * i) % 26 == 1) {
                 detinv = i;
                 break;
             }
         }
-        if(detinv==-1){
-            System.out.println("Multiplicative inverse Does Not exist for determinant "+det+" exitting");
+        if (detinv == -1) {
+            System.out.println("Multiplicative inverse Does Not exist for determinant " + det + ". Exiting");
+            System.exit(0);
         }
         return detinv;
     }
-    
+
     // Following function generates the key matrix for the key string
     static void getKeyMatrix(int keyMatrix[][]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the Key Matrix");
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                keyMatrix[i][j] = sc.nextInt()%26;
+                keyMatrix[i][j] = sc.nextInt() % 26;
             }
         }
         sc.close();
-        if(check(keyMatrix)==-1){
+        if (check(keyMatrix) == -1) {
             System.exit(0);
         }
     }
-    
-    // Following function encrypts the message
-    static void encrypt(int cipherMatrix[],
-            int keyMatrix[][],
-            int msg[]) {
-        int i, j;
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 1; j++) {
-                cipherMatrix[i] = 0;
-                cipherMatrix[i]+=msg[j]*keyMatrix[j][i];
-            }
-            cipherMatrix[i] = cipherMatrix[i] % 26;
-        }
-    }
-    static void dencrypt(int cipherMatrix[][],
-            int keyMatrix[][],
-            int messageVector[][]) {
-        int x, i, j;
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 1; j++) {
-                cipherMatrix[i][j] = 0;
 
-                for (x = 0; x < 3; x++) {
-                    cipherMatrix[i][j] += keyMatrix[i][x] * messageVector[x][j];
+    // Encrypt a plaintext message using the provided key matrix
+    //Matrix Multiplication
+    static String encrypt(String plaintext, int[][] keyMatrix) {
+        int n = keyMatrix.length;
+        int[] encrypted = new int[n];
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < plaintext.length(); i += n) {
+            for (int j = 0; j < n; j++) {
+                encrypted[j] = 0;
+                for (int k = 0; k < n; k++) {
+                    encrypted[j] += (plaintext.charAt(i + k) - 'A') * keyMatrix[k][j];
                 }
-
-                cipherMatrix[i][j] = cipherMatrix[i][j] % 26;
+                encrypted[j] %= 26;
+                result.append((char) (encrypted[j] + 'A'));
             }
         }
+
+        return result.toString();
     }
     static void printMat(int[][] mat){
         for(int i=0;i<3;i++){
@@ -116,60 +110,23 @@ class HillCipherImp {
             }
             System.out.println();
         }
+        System.out.println();
     }
-    // Function to implement Hill Cipher
-    static void HillCipher(String message, int[][] key) {
-        int m = message.length();
-        int n = m + 3-m%3;
-        char[] ch = new char[n];
-        int[] cipher = new int[n];
-        for(int i=0;i<m;i++){
-            ch[i]=message.charAt(i);
-        }
-        int[] arr = new int[3];
-        int[] chr = new int[3];
-        for(int i=0;i<n/3;i++){
-            int row = i*3;
-            arr[0]=(int)ch[row];
-            arr[1]=(int)ch[row+1];
-            arr[2]=(int)ch[row+2];
-            encrypt(chr, key, arr);
-            cipher[row]=chr[0];
-            cipher[row+1]=chr[1];
-            cipher[row+2]=chr[2];
-        }    
-        System.out.println("Cipher Text is:");
-        for(int i=0;i<m;i++){
-            System.out.print((char)(cipher[i]+'a'));
-        }
-    }
-
-    // Driver code
     public static void main(String[] args) {
-        String msg;
-        Scanner sc = new Scanner(System.in);
-        int[][] keyMatrix = new int[3][3];
-        int[][] keyinv = new int[3][3];
-        System.out.println("----------------------Hill Cipher-----------------------");
+        int keyMatrix[][] = new int[3][3];
+        int keyinv[][] = new int[3][3];
         getKeyMatrix(keyMatrix);
         getKeyinv(keyMatrix,keyinv);
-        while (true) {
-        System.out.print("Enter option:\n1.Encryption\n2.Decryption:");
-        // int choice = sc.nextInt();
-        int choice = 1;
-        switch (choice) {
-            case 1:
-                System.out.println("Enter Plaintext for Encryption");
-                msg = sc.nextLine();
-                HillCipher(msg,keyMatrix);
-                break;
-            case 2:
-                break;
-            default:
-                System.out.println("Enter correct option");
-                break;
-        }
-        sc.close();
+        System.out.println("Enter the plaintext message (in uppercase):");
+        Scanner scanner = new Scanner(System.in);
+        String plaintext = "PAYMOREMONEY";
+
+        String ciphertext = encrypt(plaintext, keyMatrix);
+        System.out.println("Encrypted Message: " + ciphertext);
+
+        String decryptedMessage = encrypt(ciphertext, keyinv);
+        System.out.println("Decrypted Message: " + decryptedMessage);
+
+        scanner.close();
     }
 }
-
